edd9dcbd3cdae4640a28021618cac8d2df5e0509
nikolaine <niko121879@gmail.com>
2023-02-22 11:13:08 -0800

    made server-side and client-side

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import client from 'apollo-client'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
